2cb419db7f0ec054d692440f58106e75a2735540
unknown <ikeda.kohei4477@gmail.com>
2018-02-13 01:12:54 +0900

    TestIntent

--- MainActivity.java
+++ MainActivity.java
@@ -1,16 +1,32 @@
-package com.example.ikego.teststorygame1;
+package com.example.ikego.testintent;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    private StoryView storyView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        storyView = new StoryView(this);
-        setContentView(storyView);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.button1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Pushed", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this,NextActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        View decor = this.getWindow().getDecorView();
+        decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
+
 }
